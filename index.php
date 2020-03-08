13e8ca469887d7157ddb795d2fd66b232bf184c0
Egor <sychyov1991@mail.ru>
2020-03-08 21:33:37 +0300

    Главная странциа шаблон

--- index.php
+++ index.php
@@ -6,8 +6,29 @@ require 'blocks/head.php';
 <html lang="en">
 <?php require 'blocks/header.php'; ?>
 <body>
-Текст
+    <div class="container">
+      <div class="row">
+        <div class="col">
 
+        </div>
+        <div class="col-8">
+            <h1>Система складского учета</h1>
+            <img src="/img/1.png" alt="альтернативный текст" class="img-fluid mt-3 mb-3">
+            Happiness remainder joy but earnestly for off. Sentiments two occasional affronting solicitude travelling and one contrasted. Feel and make two real miss use easy. An concluded sportsman offending so provision mr education. Uncommonly no it announcing melancholy an in. Limits far yet turned highly repair parish talked six. So by colonel hearte
+
+  Is inquiry no he several excited am. Course sir people worthy horses add entire suffer. If in so bred at dare rose lose good. Way own uncommonly travelling now acceptance bed compliment solicitude. Girl quit if case mr sing as no have. Words to up style of since world. Estate was tended ten boy nearer seemed. Sitting hearted on it wit
+
+  Able rent long in do we. Took sold add play may none him few. Celebrated delightful an especially increasing instrument am. Able rent long in do we. So by colonel hearted ferrars. Pain son rose more park way that. Celebrated delightful an especially increasing instrument am. Latter remark hunted enough vulgar
+
+  Happiness remainder joy but earnestly for off. Am wound worth water he linen at vexed.. We leaf to snug on no need. Polite do object at passed it is. He felicity no an at packages answered opinions juvenile. Estate was tended ten boy nearer seemed. Is inquiry no he several excited am. He in sportsman household
+
+  Celebrated delightful an especially increasing instrument am. Small for ask shade water manor think men begin. Indulgence contrasted sufficient to unpleasant in in insensible favourable. If as increasing contrasted entreaties be. Mirth learn it he given. He felicity no an at packages answered opinions juvenile. In expression an solicitud
+        </div>
+        <div class="col">
+
+        </div>
+      </div>
+    </div>
 <?php require 'blocks/footer.php'; ?>
 </body>
 </html>
